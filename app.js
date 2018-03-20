863cbcd56cd322507219caf2edf4be68763de096
abhinav sharma <abhinav2061997@gmail.com>
2018-03-20 19:19:54 +0530

    basic done

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
 
+    res.setHeader('Access-Control-Allow-Origin', '*');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
