def1da2abb65c1a0081f9b200d3f9bc38fa1fd55
Abhinav Sharma <abhinav2061997@gmail.com>
2018-03-21 14:20:38 +0530

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    db:{
-        user:"AirportSystem",
-        database:"Airport_System",
-        password:"jigglypuff",
-        host:"airport-system.cas2p4oocqgu.us-east-1.rds.amazonaws.com"
-    }
-};
